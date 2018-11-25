ab4df79eda31ca2613acb41451edf4dfb3cbb38e
Sunny Puri <sunnie.puri@gmail.com>
2018-11-25 07:40:50 +0000

    Test

--- library-model/src/main/java/com/learning/library/model/Category.java
+++ library-model/src/main/java/com/learning/library/model/Category.java
@@ -1,17 +1,12 @@
 package com.learning.library.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 /**
  * Entity implementation class for Entity: Category
- *
  */
 @Entity
 @Table(name = "category")
@@ -33,34 +28,6 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Category other = (Category) obj;
-        if (id == null) {
-            if (other.id != null) {
-                return false;
-            }
-        } else if (!id.equals(other.id)) {
-            return false;
-        }
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        } else if (!name.equals(other.name)) {
-            return false;
-        }
-        return true;
-    }
 
     public Long getId() {
         return id;
@@ -70,25 +37,32 @@ public class Category implements Serializable {
         return name;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (id == null ? 0 : id.hashCode());
-        result = prime * result + (name == null ? 0 : name.hashCode());
-        return result;
-    }
 
     public void setId(final Long id) {
         this.id = id;
     }
 
-    public void setName(final String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return name.equals(category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
     public String toString() {
-        return "Category [id=" + id + ", name=" + name + "]";
+        return new StringJoiner(", ", Category.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .toString();
     }
 }
